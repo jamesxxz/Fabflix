4f11a1ed8a59f0cd33c3041c1a2b24ea628362db
Jyun Rong Liu <jyunrl@uci.edu>
2024-10-19 15:48:34 -0700

    Implemented basic login functionality

--- cs122b-project1/WebContent/login.js
+++ cs122b-project1/WebContent/login.js
@@ -1,8 +1,24 @@
+let loginForm = jQuery("#loginForm")
 
+function handleSuccessLogin(resData) {
+    const resultData = JSON.parse(resData);
 
-function handleSuccessLogIn(response) {
-    if (response.success) {
-        console.log("success")
-        window.location.href = "index.html";
+    if (resultData.redirect) {
+        console.log("success");
+        window.location.replace("index.html");
     }
 }
+
+function handleLoginSubmit(logInEvent) {
+
+    logInEvent.preventDefault();  // prevent redirect of page
+
+    jQuery.ajax({
+        method: "POST",
+        url: "api/login",
+        data: loginForm.serialize(),
+        success: (resData) => handleSuccessLogin(resData)
+    })
+}
+
+loginForm.submit(handleLoginSubmit)
--- cs122b-project1/src/LoginServlet.java
+++ cs122b-project1/src/LoginServlet.java
@@ -6,14 +6,17 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import com.google.gson.JsonObject;
 
-@WebServlet("/customerLogin")
+@WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         // Retrieve form data
-        String email = request.getParameter("email");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
 
         JsonObject jsonResponse = new JsonObject();
         jsonResponse.addProperty("redirect", true);  // Signal the client to redirect
