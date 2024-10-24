28956db1447f30d5b8838e31e82ef2c549f590a0
Jyun Rong Liu <jyunrl@uci.edu>
2024-10-24 04:08:56 -0700

    implemented basic login flow and authenticated user with db and show error msg for invalid condition

--- cs122b-project1/WebContent/login.js
+++ cs122b-project1/WebContent/login.js
@@ -1,24 +1,30 @@
-let loginForm = jQuery("#loginForm")
+let loginForm = $("#loginForm");
 
 function handleSuccessLogin(resData) {
     const resultData = JSON.parse(resData);
 
-    if (resultData.redirect) {
-        console.log("success");
+    if (resultData["status"] === "success") {
         window.location.replace("index.html");
+    } else {
+        // If login fails, the web page will display
+        // error messages on <div> with id "login_error_message"
+        $("#login_error_message").text(resultData["message"]);
     }
+
 }
 
 function handleLoginSubmit(logInEvent) {
 
     logInEvent.preventDefault();  // prevent redirect of page
-
-    jQuery.ajax({
-        method: "POST",
-        url: "api/login",
-        data: loginForm.serialize(),
-        success: (resData) => handleSuccessLogin(resData)
-    })
+    console.log(loginForm.serialize());
+    $.ajax(
+        "api/login", {
+            method: "POST",
+            // Serialize the login form to the data sent by POST request
+            data: loginForm.serialize(),
+            success: handleSuccessLogin
+        }
+    );
 }
 
 loginForm.submit(handleLoginSubmit)
--- cs122b-project1/src/LoginServlet.java
+++ cs122b-project1/src/LoginServlet.java
@@ -1,32 +1,96 @@
-import jakarta.servlet.ServletException;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.Objects;
 
-import com.google.gson.JsonObject;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 3L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         // Retrieve form data
-        String username = request.getParameter("username");
+        String username = request.getParameter("email");
         String password = request.getParameter("password");
 
-        JsonObject jsonResponse = new JsonObject();
-        jsonResponse.addProperty("redirect", true);  // Signal the client to redirect
-        jsonResponse.addProperty("redirectUrl", "movielist.html");  // The URL to redirect to
-        PrintWriter out = response.getWriter();
+        String customerId = "";
+        String validPassword = "";
+        boolean isSuccess = false;
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "SELECT * from customers where email = ? ";
 
-        out.write(jsonResponse.toString());
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
 
-        response.setStatus(200);
-        out.close();
+            if (rs.next()) {
+                customerId = rs.getString("id");
+                validPassword = rs.getString("password");
+                isSuccess = Objects.equals(password, validPassword);
+            }
+
+        } catch (Exception e) {
+
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            request.getServletContext().log("Error:", e);
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+
+
+        JsonObject responseJsonObject = new JsonObject();
+
+        if (isSuccess && !Objects.equals(password, "")) {
+            // Login success:
+
+            // set this user into the session
+            request.getSession().setAttribute("user", new User(customerId, username));
+
+            responseJsonObject.addProperty("status", "success");
+            responseJsonObject.addProperty("message", "success");
+
+        } else {
+            // Login fail
+            responseJsonObject.addProperty("status", "fail");
+            // Log to localhost log
+            request.getServletContext().log("Login failed");
+            // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
+            if (Objects.equals(customerId, "")) {
+                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+            } else if (!isSuccess) {
+                responseJsonObject.addProperty("message", "incorrect password");
+            }
+        }
+        response.getWriter().write(responseJsonObject.toString());
     }
 }
 
