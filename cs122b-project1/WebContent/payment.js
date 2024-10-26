804bf2b4f84962fa862320e0a13edde3fb9bfda2
DarryDarius <104342794+DarryDarius@users.noreply.github.com>
2024-10-25 23:03:09 -0700

    implemented the confirmation.html and payment.html

---
+++ cs122b-project1/WebContent/payment.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    let cartItems = JSON.parse(sessionStorage.getItem("shoppingCart")) || [];
+    let totalPrice = 0;
+
+
+    cartItems.forEach(item => {
+        totalPrice += item.price * item.quantity;
+    });
+
+
+    $("#totalPrice").text(`$${totalPrice.toFixed(2)}`);
+});
+
+let payment_form = $("#payment_form");
+
+function handlePaymentResult(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
+    console.log("handle payment response", resultDataJson);
+
+    if (resultDataJson["status"] === "success") {
+        window.location.replace("confirmation.html");
+    } else {
+
+        console.log("show error message", resultDataJson["message"]);
+        $("#payment_error_message").text(resultDataJson["message"]);
+    }
+}
+
+function submitPaymentForm(formSubmitEvent) {
+    console.log("submit Payment form");
+    formSubmitEvent.preventDefault();
+
+    $.ajax({
+        url: "api/payment",
+        method: "POST",
+        data: payment_form.serialize(),
+        success: handlePaymentResult
+    });
+}
+
+payment_form.submit(submitPaymentForm);
