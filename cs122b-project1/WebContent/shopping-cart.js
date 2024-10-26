2b490a68dd625f35f41eed32d995caa0968d7e69
Jyun Rong Liu <jyunrl@uci.edu>
2024-10-26 16:05:42 -0700

    Implemented delte, add/minus quantity functionality for shopping cart

--- cs122b-project1/WebContent/shopping-cart.js
+++ cs122b-project1/WebContent/shopping-cart.js
@@ -8,14 +8,41 @@ function handleResult() {
         let rowHTML = "";
         rowHTML += "<tr>";
         rowHTML += "<th>" + movieTitle + "</th>";
-        rowHTML += "<th>" + movieQuantity + "</th>";
+        rowHTML += `<th style="display: flex; gap: 20px"><a style="background-color: white; color: black; border-radius: 5px" onclick="addMovies('${movieTitle}')">&nbsp;+&nbsp;</a>`
+            + movieQuantity
+            + `<a style="background-color: white; color: black; border-radius: 5px" onclick="minusMovies('${movieTitle}')">&nbsp;-&nbsp;</a></th>`;
         rowHTML += "<th>" + "$20" + "</th>";
         rowHTML += "<th>" + "$" + movieQuantity * 20 + "</th>";
-        rowHTML += `<th><button>Delete</button></th>`;
+        rowHTML += `<th><button onclick="deleteMovie('${movieTitle}')">Delete</button></th>`;
         rowHTML += "</tr>";
 
         // Append the row created to the table body, which will refresh the page
+        console.log(rowHTML);
         cartTableElement.append(rowHTML);
+    }
+}
+
+function deleteMovie(movieTitle) {
+    let allMoviesInCart = JSON.parse(sessionStorage.getItem("moviesInCart"));
+    const {[movieTitle]: _, ...newAllMovies} = allMoviesInCart;
+    console.log(newAllMovies);
+    sessionStorage.setItem("moviesInCart", JSON.stringify(newAllMovies));
+    window.location.reload();
+}
+
+function addMovies(movieTitle) {
+    let allMoviesInCart = JSON.parse(sessionStorage.getItem("moviesInCart"));
+    allMoviesInCart[movieTitle] += 1;
+    sessionStorage.setItem("moviesInCart", JSON.stringify(allMoviesInCart));
+    window.location.reload();
+}
+
+function minusMovies(movieTitle) {
+    let allMoviesInCart = JSON.parse(sessionStorage.getItem("moviesInCart"));
+    if (allMoviesInCart[movieTitle] > 0) {
+        allMoviesInCart[movieTitle] -= 1;
+        sessionStorage.setItem("moviesInCart", JSON.stringify(allMoviesInCart));
+        window.location.reload();
     }
 }
 
